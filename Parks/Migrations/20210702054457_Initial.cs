478d537951e4ca2769fbc33ad959d1308e7e0e47
Laurie Waller <lauriewaller7@gmail.com>
2021-07-01 22:48:33 -0700

    create initial migration

---
+++ Parks/Migrations/20210702054457_Initial.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParkApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Parks",
+                columns: table => new
+                {
+                    ParkId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    ParkType = table.Column<string>(type: "varchar(1000) CHARACTER SET utf8mb4", maxLength: 1000, nullable: false),
+                    Location = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parks", x => x.ParkId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Location", "Name", "ParkType" },
+                values: new object[,]
+                {
+                    { 1, "Montana", "Glacier National Park", "National" },
+                    { 2, "California", "Death Valley National Park", "National" },
+                    { 3, "Washington", "Olympic National Park", "National" },
+                    { 4, "Washington", "North Cascades National Park", "National" },
+                    { 5, "California", "Sequoia National Park", "National" },
+                    { 6, "Oregon", "Milo McIver State Park", "State" },
+                    { 7, "Oregon", "Silver Falls Park", "State" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Parks");
+        }
+    }
+}
